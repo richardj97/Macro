215b0f87b5701aaef4d790e33ccdf2c41a690798
Richard Jacobs <richardjacobs97@hotmail.co.uk>
2018-12-05 19:49:00 +0000

    Add files via upload

---
+++ Silence.Macro/MacroEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace Silence.Macro
+{
+    /// <summary>
+    /// Represents an event that occurs during macro recording.
+    /// </summary>
+    public abstract class MacroEvent
+    {
+        /// <summary>
+        /// Returns a representation of this object as an XML string.
+        /// </summary>
+        /// <returns></returns>
+        public abstract string ToXml();
+    }
+}
